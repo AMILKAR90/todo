dcca520c5758815095704c0411e529316d3730c8
Amilkar90 <amilkaromero1@gmail.com>
2022-01-09 18:23:15 -0500

    add entity

---
+++ todo.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace todo.Functions.Entities
+{
+    public class TodoEntity :TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ todo.common/Models/Tarea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace todo.common.Models
+{
+    public class Tarea
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
